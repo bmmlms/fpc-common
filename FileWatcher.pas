305959b0288bbc48ca00d6d2478a92327ee9a172
bmmlms <bmmlms@users.noreply.github.com>
2012-10-25 19:43:22 +0000

    - Fixed FileWatcher
    git-svn-id: svn://mistake.ws/common@179 1248ac36-ef73-4b16-a0d7-831ea8293731

--- FileWatcher.pas
+++ FileWatcher.pas
@@ -10,12 +10,10 @@ type
 
   TFileWatcher = class(TThread)
   private
-    FWatchHandle: DWORD;
     FPath: string;
     FFilename: string;
     FFilenameNew: string;
     FAction: DWORD;
-    FFileEvent: THandle;
     FFilter: DWORD;
     FTermEvent: TEvent;
 
@@ -24,7 +22,6 @@ type
     procedure TriggerEvent;
   public
     constructor Create(const Path: string; Filter: DWORD);
-    destructor Destroy; override;
     procedure Execute; override;
     procedure Terminate; reintroduce;
 
@@ -56,6 +53,7 @@ end;
 
 procedure TFileWatcher.Execute;
 var
+  WatchHandle: DWORD;
   Buffer: Pointer;
   BufLen: DWORD;
   Read: DWORD;
@@ -65,73 +63,88 @@ var
   Overlap: TOverlapped;
   WaitResult: DWORD;
   EventArray: array [0..1] of THandle;
+  FileEvent: TEvent;
 begin
-  FWatchHandle := CreateFile(PChar(FPath), FILE_LIST_DIRECTORY or GENERIC_READ,
-    FILE_SHARE_READ or FILE_SHARE_WRITE or FILE_SHARE_DELETE, nil,
-    OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS or FILE_FLAG_OVERLAPPED, 0);
-
-  if (FWatchHandle = INVALID_HANDLE_VALUE) or (FWatchHandle = 0) then
-    Exit;
-
-  FFileEvent := CreateEvent(nil, FALSE, FALSE, nil);
-  Overlap.hEvent := FFileEvent;
-
-  FTermEvent := TEvent.Create(nil, False, False, IntToStr(Handle) + 'Term');
-
-  EventArray[0] := FFileEvent;
-  EventArray[1] := FTermEvent.Handle;
-
-  BufLen := 65535;
-  Buffer := AllocMem(BufLen);
   try
-    while not Terminated do
-    begin
-      Read := 0;
-      if ReadDirectoryChangesW(FWatchHandle, Buffer, BufLen, True, FFilter, @Read, @Overlap, nil) then
+    WatchHandle := CreateFile(PChar(FPath), FILE_LIST_DIRECTORY or GENERIC_READ,
+      FILE_SHARE_READ or FILE_SHARE_WRITE or FILE_SHARE_DELETE, nil,
+      OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS or FILE_FLAG_OVERLAPPED, 0);
+
+    if (WatchHandle = INVALID_HANDLE_VALUE) or (WatchHandle = 0) then
+      Exit;
+
+    FileEvent := TEvent.Create(nil, False, False, '');
+    Overlap.hEvent := FileEvent.Handle;
+
+    FTermEvent := TEvent.Create(nil, False, False, '');
+
+    EventArray[0] := FileEvent.Handle;
+    EventArray[1] := FTermEvent.Handle;
+
+    BufLen := 65535;
+    Buffer := AllocMem(BufLen);
+    try
+      while not Terminated do
       begin
-        WaitResult := WaitForMultipleObjects(2, @EventArray, False, INFINITE);
-        case WaitResult of
-          WaitDir:
-            begin
-              Info := Buffer;
-              repeat
-                NextOffset := Info.NextEntryOffset;
-                FAction := Info.Action;
-                NameLen := Info.FilenameLength;
+        Read := 0;
 
-                // Wenn man im Explorer die File-Properties anzeigt und ID3-Tags ändert,
-                // löscht der die Datei und stellt sie dann wieder her. Das Sleep() ist doof,
-                // aber mir fällt auf die Schnelle nichts besseres ein...
-                if FAction = FILE_ACTION_REMOVED then
-                  Sleep(100);
+        if ReadDirectoryChangesW(WatchHandle, Buffer, BufLen, True, FFilter, @Read, @Overlap, nil) then
+        begin
+          WaitResult := WaitForMultipleObjects(2, @EventArray, False, INFINITE);
+          case WaitResult of
+            WaitDir:
+              begin
+                Info := Buffer;
+                repeat
+                  NextOffset := Info.NextEntryOffset;
+                  FAction := Info.Action;
+                  NameLen := Info.FilenameLength;
 
-                // Die letzte Bedingung ist für MusicBee. Da wird die Datei umbenannt und dann neu angelegt,
-                // das ignorieren wir dann.
-                if (((FAction = FILE_ACTION_REMOVED) and (not FileExists(FPath + WideCharLenToString(@Info.FileName, NameLen div 2)))) or
-                     (FAction <> FILE_ACTION_REMOVED) and
-                    not ((FAction = FILE_ACTION_ADDED) and (FPath + WideCharLenToString(@Info.FileName, NameLen div 2) = FFilename)))  then
-                begin
-                  if FAction <> FILE_ACTION_RENAMED_NEW_NAME then
-                    FFilename := WideCharLenToString(@Info.FileName, NameLen div 2)
-                  else
-                    FFilenameNew := WideCharLenToString(@Info.Filename, NameLen div 2);
+                  // Wenn man im Explorer die File-Properties anzeigt und ID3-Tags ändert,
+                  // löscht der die Datei und stellt sie dann wieder her. Das Sleep() ist doof,
+                  // aber mir fällt auf die Schnelle nichts besseres ein...
+                  if FAction = FILE_ACTION_REMOVED then
+                    Sleep(100);
 
-                  if Assigned(FOnEvent) then
-                    Synchronize(TriggerEvent);
-                end;
+                  // Die letzte Bedingung ist für MusicBee. Da wird die Datei umbenannt und dann neu angelegt,
+                  // das ignorieren wir dann.
+                  if (((FAction = FILE_ACTION_REMOVED) and (not FileExists(FPath + WideCharLenToString(@Info.FileName, NameLen div 2)))) or
+                       (FAction <> FILE_ACTION_REMOVED) and
+                      not ((FAction = FILE_ACTION_ADDED) and (FPath + WideCharLenToString(@Info.FileName, NameLen div 2) = FFilename))) then
+                  begin
+                    if FAction <> FILE_ACTION_RENAMED_NEW_NAME then
+                      FFilename := WideCharLenToString(@Info.FileName, NameLen div 2)
+                    else
+                      FFilenameNew := WideCharLenToString(@Info.Filename, NameLen div 2);
 
-                Info := Pointer(Int64(Info) + NextOffset);
-              until NextOffset = 0;
-            end;
-          WaitTerm:
-            Terminate;
-        else
-          Break;
+                    if Assigned(FOnEvent) then
+                      Synchronize(TriggerEvent);
+                  end;
+
+                  Info := Pointer(Int64(Info) + NextOffset);
+
+                  if Terminated then
+                    Break;
+
+                until NextOffset = 0;
+              end;
+            WaitTerm:
+              begin
+                Break;
+              end
+          else
+            Break;
+          end;
         end;
       end;
+    finally
+      CloseHandle(WatchHandle);
+      FileEvent.Free;
+      FTermEvent.Free;
+      FreeMem(Buffer, BufLen);
     end;
-  finally
-    FreeMem(Buffer, BufLen);
+  except
+
   end;
 end;
 
@@ -139,6 +152,7 @@ procedure TFileWatcher.Terminate;
 begin
   if FTermEvent <> nil then
     FTermEvent.SetEvent;
+
   inherited;
 end;
 
@@ -146,17 +160,6 @@ procedure TFileWatcher.TriggerEvent;
 begin
   if Assigned(FOnEvent) then
     FOnEvent(Self, FAction, FFilename, FFilenameNew);
-end;
-
-destructor TFileWatcher.Destroy;
-begin
-  if FWatchHandle <> INVALID_HANDLE_VALUE then
-    CloseHandle(FWatchHandle);
-
-  CloseHandle(FFileEvent);
-  FTermEvent.Free;
-
-  inherited;
 end;
 
 end.
