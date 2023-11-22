c9d954881f2ce13a6593a3788e4113bce78b030c
bmmlms <bmmlms@users.noreply.github.com>
2023-11-22 21:02:40 +0100

    Added AudioEndpointNotificationListener

---
+++ AudioEndpointNotificationListener.pas
@@ -0,0 +1,182 @@
+unit AudioEndpointNotificationListener;
+
+interface
+
+uses
+  ActiveX,
+  Classes,
+  ComObj,
+  InterfaceBase,
+  SysUtils,
+  Windows;
+
+type
+  EDataFlow = (eRender, eCapture, eAll, EDataFlow_enum_count);
+  ERole = (eConsole, eMultimedia, eCommunications, ERole_enum_count);
+
+  TPropertyKey = record
+    fmtid: TGUID;
+    pid: DWORD;
+  end;
+
+  { IPropertyStore }
+
+  IPropertyStore = interface(IUnknown)
+    function GetCount(out cProps: DWORD): HRESULT; stdcall;
+    function GetAt(iProp: DWORD; out key: TPropertyKey): HRESULT; stdcall;
+    function GetValue(const key: TPropertyKey; out Value: TPropVariant): HRESULT; stdcall;
+  end;
+
+  { IMMDevice }
+
+  IMMDevice = interface(IUnknown)
+    ['{D666063F-1587-4E43-81F1-B948E807363F}']
+    function Activate(const iid: TGUID; dwClsCtx: DWORD; pActivationParams: PPropVariant; out EndpointVolume: IUnknown): HRESULT; stdcall;
+    function OpenPropertyStore(stgmAccess: DWORD; out Properties: IPropertyStore): HRESULT; stdcall;
+    function GetId(out strId: LPWSTR): HRESULT; stdcall;
+    function GetState(out State: DWORD): HRESULT; stdcall;
+  end;
+
+  { IMMDeviceCollection }
+
+  IMMDeviceCollection = interface(IUnknown)
+    ['{0BD7A1BE-7A1A-44DB-8397-CC5392387B5E}']
+    function GetCount(out cDevices: UINT): HRESULT; stdcall;
+    function Item(nDevice: UINT; out Device: IMMDevice): HRESULT; stdcall;
+  end;
+
+  { IMMNotificationClient }
+
+  IMMNotificationClient = interface(IUnknown)
+    ['{7991EEC9-7E89-4D85-8390-6C703CEC60C0}']
+    function OnDeviceStateChanged(pwstrDeviceId: LPWSTR; dwNewState: DWORD): HRESULT; stdcall;
+    function OnDeviceAdded(pwstrDeviceId: LPWSTR): HRESULT; stdcall;
+    function OnDeviceRemoved(pwstrDeviceId: LPWSTR): HRESULT; stdcall;
+    function OnDefaultDeviceChanged(flow: EDataFlow; role: ERole; pwstrDefaultDeviceId: LPWSTR): HRESULT; stdcall;
+    function OnPropertyValueChanged(pwstrDeviceId: LPWSTR; const key: TPropertyKey): HRESULT; stdcall;
+  end;
+
+  { IMMDeviceEnumerator }
+
+  IMMDeviceEnumerator = interface(IUnknown)
+    ['{A95664D2-9614-4F35-A746-DE8DB63617E6}']
+    function EnumAudioEndpoints(dataFlow: EDataFlow; dwStateMask: DWORD; out Devices: IMMDeviceCollection): HRESULT; stdcall;
+    function GetDefaultAudioEndpoint(EDF: EDataFlow; role: ERole; out EndPoint: IMMDevice): HRESULT; stdcall;
+    function GetDevice(pwstrId: LPWSTR; out EndPoint: IMMDevice): HRESULT; stdcall;
+    function RegisterEndpointNotificationCallback(const Client: IMMNotificationClient): HRESULT; stdcall;
+    function UnregisterEndpointNotificationCallback(const Client: IMMNotificationClient): HRESULT; stdcall;
+  end;
+
+  { TAudioEndpointNotificationListener }
+
+  TAudioEndpointNotificationListener = class(TInterfacedObject, IMMNotificationClient)
+  const
+    CLASS_IMMDeviceEnumerator: TGUID = '{BCDE0395-E52F-467C-8E3D-C4579291692E}';
+    WM_DEFAULT_DEVICE_CHANGED = WM_USER + 47;
+  private
+    FWndHandle: HWND;
+    FMMDeviceEnumerator: IMMDeviceEnumerator;
+    FOnDefaultDeviceChanged: TNotifyEvent;
+
+    procedure WndProc(var Msg: TMessage);
+  protected
+    function OnDeviceStateChanged(pwstrDeviceId: LPWSTR; dwNewState: DWORD): HRESULT; stdcall;
+    function OnDeviceAdded(pwstrDeviceId: LPWSTR): HRESULT; stdcall;
+    function OnDeviceRemoved(pwstrDeviceId: LPWSTR): HRESULT; stdcall;
+    function OnDefaultDeviceChangedInternal(flow: EDataFlow; role: ERole; pwstrDefaultDeviceId: LPWSTR): HRESULT; stdcall;
+    function OnPropertyValueChanged(pwstrDeviceId: LPWSTR; const key: TPropertyKey): HRESULT; stdcall;
+
+    function IMMNotificationClient.OnDefaultDeviceChanged = OnDefaultDeviceChangedInternal;
+  public
+    destructor Destroy; override;
+
+    procedure Start;
+    procedure Stop;
+
+    property OnDefaultDeviceChanged: TNotifyEvent read FOnDefaultDeviceChanged write FOnDefaultDeviceChanged;
+  end;
+
+implementation
+
+{ TDeviceNotificationListener }
+
+destructor TAudioEndpointNotificationListener.Destroy;
+begin
+  Stop;
+
+  inherited Destroy;
+end;
+
+procedure TAudioEndpointNotificationListener.Start;
+begin
+  if FWndHandle <> 0 then
+    Exit;
+
+  FWndHandle := WidgetSet.AllocateHWnd(WndProc);
+  if FWndHandle = 0 then
+    raise Exception.Create('AllocateHWnd() failed: %s'.Format([SysErrorMessage(GetLastError)]));
+
+  FMMDeviceEnumerator := CreateComObject(CLASS_IMMDeviceEnumerator) as IMMDeviceEnumerator;
+
+  if not Succeeded(FMMDeviceEnumerator.RegisterEndpointNotificationCallback(Self)) then
+  begin
+    FreeAndNil(FMMDeviceEnumerator);
+    WidgetSet.DeallocateHWnd(FWndHandle);
+    FWndHandle := 0;
+    raise Exception.Create('RegisterEndpointNotificationCallback() failed');
+  end;
+end;
+
+procedure TAudioEndpointNotificationListener.Stop;
+begin
+  if Assigned(FMMDeviceEnumerator) then
+    FMMDeviceEnumerator.UnregisterEndpointNotificationCallback(Self);
+
+  if FWndHandle <> 0 then
+  begin
+    WidgetSet.DeallocateHWnd(FWndHandle);
+    FWndHandle := 0;
+  end;
+end;
+
+procedure TAudioEndpointNotificationListener.WndProc(var Msg: TMessage);
+begin
+  if Msg.msg = WM_DEFAULT_DEVICE_CHANGED then
+  begin
+    if Assigned(FOnDefaultDeviceChanged) then
+      FOnDefaultDeviceChanged(Self);
+
+    Msg.Result := 0;
+  end else
+    Msg.Result := DefWindowProc(FWndHandle, Msg.Msg, Msg.WParam, Msg.LParam);
+end;
+
+function TAudioEndpointNotificationListener.OnDeviceStateChanged(pwstrDeviceId: LPWSTR; dwNewState: DWORD): HRESULT; stdcall;
+begin
+  Result := S_OK;
+end;
+
+function TAudioEndpointNotificationListener.OnDeviceAdded(pwstrDeviceId: LPWSTR): HRESULT; stdcall;
+begin
+  Result := S_OK;
+end;
+
+function TAudioEndpointNotificationListener.OnDeviceRemoved(pwstrDeviceId: LPWSTR): HRESULT; stdcall;
+begin
+  Result := S_OK;
+end;
+
+function TAudioEndpointNotificationListener.OnDefaultDeviceChangedInternal(flow: EDataFlow; role: ERole; pwstrDefaultDeviceId: LPWSTR): HRESULT; stdcall;
+begin
+  if (Flow = eRender) and (role = eMultimedia) then
+    PostMessage(FWndHandle, WM_DEFAULT_DEVICE_CHANGED, 0, 0);
+
+  Result := S_OK;
+end;
+
+function TAudioEndpointNotificationListener.OnPropertyValueChanged(pwstrDeviceId: LPWSTR; const key: TPropertyKey): HRESULT; stdcall;
+begin
+  Result := S_OK;
+end;
+
+end.
