60b7f403f108ddb4d6f04cf0897c522f6347fe3a
bmmlms <bmmlms@users.noreply.github.com>
2026-05-15 16:25:09 +0100

    Add thread suspension and threaded message box to TExceptionHandler

--- ExceptionHandler.pas
+++ ExceptionHandler.pas
@@ -43,8 +43,77 @@ type
 
 implementation
 
+const
+  TH32CS_SNAPTHREAD = $00000004;
+  THREAD_SUSPEND_RESUME = $0002;
+
+type
+  TThreadEntry32 = record
+    dwSize: DWORD;
+    cntUsage: DWORD;
+    th32ThreadID: DWORD;
+    th32OwnerProcessID: DWORD;
+    tpBasePri: Longint;
+    tpDeltaPri: Longint;
+    dwFlags: DWORD;
+  end;
+
+function CreateToolhelp32Snapshot(dwFlags, th32ProcessID: DWORD): THandle; stdcall; external kernel32;
+function Thread32First(hSnapshot: THandle; var lpte: TThreadEntry32): BOOL; stdcall; external kernel32;
+function Thread32Next(hSnapshot: THandle; var lpte: TThreadEntry32): BOOL; stdcall; external kernel32;
+function OpenThread(dwDesiredAccess: DWORD; bInheritHandle: BOOL; dwThreadId: DWORD): THandle; stdcall; external kernel32;
+
 var
   DesktopDir, ProductVersion: string;
+
+procedure SuspendOtherThreads;
+var
+  Snapshot: THandle;
+  Entry: TThreadEntry32;
+  ThreadHandle: THandle;
+  CurrentThreadID, CurrentProcessID: DWORD;
+begin
+  CurrentThreadID := GetCurrentThreadId;
+  CurrentProcessID := GetCurrentProcessId;
+
+  Snapshot := CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+  if Snapshot <> INVALID_HANDLE_VALUE then
+  begin
+    Entry.dwSize := SizeOf(TThreadEntry32);
+    if Thread32First(Snapshot, Entry) then
+      repeat
+        if (Entry.th32OwnerProcessID = CurrentProcessID) and (Entry.th32ThreadID <> CurrentThreadID) then
+        begin
+          ThreadHandle := OpenThread(THREAD_SUSPEND_RESUME, False, Entry.th32ThreadID);
+          if ThreadHandle <> 0 then
+          begin
+            SuspendThread(ThreadHandle);
+            CloseHandle(ThreadHandle);
+          end;
+        end;
+      until not Thread32Next(Snapshot, Entry);
+    CloseHandle(Snapshot);
+  end;
+end;
+
+function MsgBoxThread(Text: PChar): ptrint;
+begin
+  MessageBox(0, Text, 'Error', MB_ICONERROR);
+  Result := 0;
+end;
+
+procedure ThreadedMsgBox(const Text: string);
+var
+  ThreadHandle: THandle;
+  ThreadID: TThreadID;
+begin
+  ThreadHandle := BeginThread(@MsgBoxThread, PChar(Text), ThreadID);
+  if ThreadHandle <> 0 then
+  begin
+    WaitForSingleObject(ThreadHandle, INFINITE);
+    CloseHandle(ThreadHandle);
+  end;
+end;
 
 procedure UnhandledException(Obj: TObject; Addr: Pointer; FrameCount: Longint; Frames: PPointer);
 const
@@ -53,6 +122,8 @@ var
   i: LongInt;
   SL: TStringList;
 begin
+  SuspendOtherThreads;
+
   SL := TStringList.Create;
   try
     SL.Add('Version: %s (%s)'.Format([ProductVersion, Architecture]));
@@ -64,9 +135,9 @@ begin
 
     try
       SL.SaveToFile(ConcatPaths([DesktopDir, 'crash_%s.txt'.Format([FormatDateTime('hhnnss', Now)])]));
-      TFunctions.MsgBox('An unhandled exception occurred and the application needs to exit. A file containing information about the crash was saved to the desktop.', 'Error', MB_ICONERROR);
+      ThreadedMsgBox('An unhandled exception occurred and the application needs to exit. A file containing information about the crash was saved to the desktop.');
     except
-      TFunctions.MsgBox('An unhandled exception occurred and the application needs to exit.'#13#10'%s'.Format([SL.Text]), 'Error', MB_ICONERROR);
+      ThreadedMsgBox('An unhandled exception occurred and the application needs to exit.'#13#10'%s'.Format([SL.Text]));
     end;
   finally
     SL.Free;
